202b2bd48b7c9cfcc1203ceade05deaabc9c4b9d
shree-vidya <hmshreevidya@gmail.com>
2021-05-27 12:22:41 +0530

    added server side

--- android/app/src/main/kotlin/com/example/bon_voyage/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bon_voyage
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants/colors.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color kPurple = Color(0xFF7B1FA2);
-Color kPink = Color(0xFFBF185E);
