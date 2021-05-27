dda8414480a33ae3ad49dc27b2ba866f3d07f5c5
Shivanirao2000 <shivanirao2000@gmail.com>
2021-05-27 18:12:05 +0530

    app2

--- Server-Side/app2.js
+++ Server-Side/app2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+var request = require('request');
 
 
 app.use(bodyParser.urlencoded({extended: "false"}));
@@ -10,5 +10,20 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send('Hello World!')
   });
+  app.get('/get-distance', (req, res) => {
+    const state = req.params.state;
+    request({
+        url:'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Red+Fort&destinations=Taj+Mahal&mode=driving&language=fr-FR&key=AIzaSyCS90XB-jQMIhQbA2C9vzfWKETNaxpjWJo',
+        method: 'GET',
+    } ,function(error,resp, body){
+            if(!error && resp.statusCode == 200){
+                 var body= JSON.parse(body);
+        
+                res.status(200).json({
+                body
+            });
+        }
+    })
+  })
 
   module.exports = app;
