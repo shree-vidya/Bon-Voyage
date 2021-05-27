db19effcb98f362d672d6bee0dbb7c083319cccd
shree-vidya <hmshreevidya@gmail.com>
2021-05-27 13:19:42 +0530

    added request package

--- Server-Side/app1.js
+++ Server-Side/app1.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var request = require('request');
 
 
 app.use(bodyParser.urlencoded({extended: "false"}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/get-cities/:state', (req, res) => {
+    const state = req.params.state;
+    request({
+        url:'https://api.countrystatecity.in/v1/countries/IN/states/'+state+'/cities',
+        method: 'GET',
+        headers: 
+        {"X-CSCAPI-KEY": "NElkaEFZNlQxY3NBZ0NGaVFsVzN6a3RlUDNyblA3R3d4bDBkaEwwTA=="}
+    } ,function(error,resp, body){
+            if(!error && resp.statusCode == 200){
+                 var body= JSON.parse(body);
+        
+                res.status(200).json({
+                body
+            });
+        }
+    })
   })
 
   module.exports = app;
