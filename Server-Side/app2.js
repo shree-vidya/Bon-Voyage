3d04c4b76b263e45503541b0b989a0396821063c
Shivanirao2000 <shivanirao2000@gmail.com>
2021-05-28 20:31:57 +0530

    added tsp

--- Server-Side/app2.js
+++ Server-Side/app2.js
@@ -2,69 +2,62 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var request = require('request');
+const wait=ms=>new Promise(resolve => setTimeout(resolve, ms));
 
-var result={
-  "ans": [
-      {
-          "name": "Sri Chamarajendra Zoological Gardens",
-          "id": "ChIJlXcOBCNwrzsRy79sy0wzV-o"
-      },
-      {
-          "name": "Mysuru Rail Museum",
-          "id": "ChIJ0zzdpXVwrzsRSsTggeRbyzE"
-      },
-      {
-          "name": "Mysore Sand Sculpture Museum",
-          "id": "ChIJpfe0LipwrzsRMSBPsnzs9Kk"
-      },
-      {
-          "name": "Regional Museum of Natural History Mysore",
-          "id": "ChIJM2ikQZ16rzsR05jLoAu4bPg"
-      },
-      {
-          "name": "Folklore Museum",
-          "id": "ChIJNbbbd5Z6rzsREcdCXBECuZg"
-      },
-      {
-          "name": "Aadumalleshwara",
-          "id": "ChIJGQfPHzl0ujsRXks3TtLntTY"
-      },
-      {
-          "name": "Sri Maharshi Valmiki Museum",
-          "id": "ChIJCWxqdld0ujsRhh1vrVRJv9M"
-      },
-      {
-          "name": "Bangalore Palace",
-          "id": "ChIJN1ZKKUkWrjsRzxIVM363-LE"
-      },
-      {
-          "name": "HAL Heritage Centre and Aerospace Museum",
-          "id": "ChIJg2Ir7MMTrjsR9kqAcrWsFew"
-      },
-      {
-          "name": "Indian Military Museum",
-          "id": "ChIJXx3q3WsWrjsR_Qw3odPlMXM"
-      }
-  ]
-};
-
-// console.log(result['ans']);
-
-function myfunc(item){
-  // console.log('place_id:'+item['id']+'|');
-  return "place_id:"+item['id']+"|";
-}
-// console.log(result['ans'].forEach(myfunc));
-// result['ans'].forEach(myfunc)
-
-var n=result['ans'].length;
+var ans;
+var placeIds=[];
 var arr = [];
-for(let i = 0; i < n; i++) {
-    arr[i] = [];
-    for(let j = 0; j < n; j++) {
-        arr[i][j] = -1;
+var visited=[];
+var result=[];
+var cost=0;
+// console.log(result['ans']);
+function myfunc(placeid1,placeid2,i,j){
+  request({
+    // url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:ChIJ74XAbuL8DDkR-wJQ-vQyHkQ|place_id:ChIJDwI0rC13dDkRs9I9GN3k9Yc|place_id:ChIJbf8C1yFxdDkR3n12P4DkKt0&destinations=place_id:ChIJ74XAbuL8DDkR-wJQ-vQyHkQ|place_id:ChIJDwI0rC13dDkRs9I9GN3k9Yc|place_id:ChIJbf8C1yFxdDkR3n12P4DkKt0&mode=driving&language=fr-FR&key=AIzaSyCS90XB-jQMIhQbA2C9vzfWKETNaxpjWJo',
+    url:'https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:'+placeid1+'&destinations=place_id:'+placeid2+'&mode=driving&language=fr-FR&key=AIzaSyCS90XB-jQMIhQbA2C9vzfWKETNaxpjWJo',
+    method: 'GET',
+} ,function(error,resp, body){
+        if(!error && resp.statusCode == 200){
+             var body= JSON.parse(body);
+            // console.log(body['rows'][0]['elements'][0]);
+            // ans=body['rows'][0]['elements'][0]['distance']['value'];
+            arr[i][j]=arr[j][i]=body['rows'][0]['elements'][0]['distance']['value'];
+            // console.log(arr[i][j]);
+            
     }
+})
 }
+
+function least(c,n){
+  var i,nc=999;
+  var min=999,kmin;
+  for(i=0;i<n;i++){
+    if((arr[c][i]!=0)&&(visited[i]==0)){
+      if(arr[c][i]<min){
+        min=arr[i][0]+arr[c][i];
+        kmin=arr[c][i];
+        nc=i;
+      }
+    }
+  }
+  if(min!=999){
+    cost+=kmin;
+  }
+  return nc;
+}
+function tsp(city,n,placeIds){
+  var ncity;
+  visited[city]=1;
+  result.push(placeIds[city]);
+  ncity=least(city,n);
+  if(ncity==999){
+    ncity=0;
+    cost+=arr[city][ncity];
+    return;
+  }
+  tsp(ncity,n,placeIds);
+}
+
 
 app.use(bodyParser.urlencoded({extended: "false"}));
 app.use(bodyParser.json());
@@ -73,28 +66,38 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   });
   app.get('/get-distance', (req, res) => {
-    const state = req.params.state;
+    placeIds = JSON.parse(req.query.placeIds);
+    var n=placeIds.length;
+    for(let i = 0; i < n; i++) {
+      arr[i] = [];
+      for(let j = 0; j < n; j++) {
+          arr[i][j] = -1;
+      }
+  }
+  for(i=0;i<n;i++){
+    visited[i]=0;
+  }
+  
     for(let i = 0; i < n; i++) {
       for(let j = 0; j < n; j++) {
-          if(arr[i][j] == -1){
-            request({
-              url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:ChIJ74XAbuL8DDkR-wJQ-vQyHkQ|place_id:ChIJDwI0rC13dDkRs9I9GN3k9Yc|place_id:ChIJbf8C1yFxdDkR3n12P4DkKt0&destinations=place_id:ChIJ74XAbuL8DDkR-wJQ-vQyHkQ|place_id:ChIJDwI0rC13dDkRs9I9GN3k9Yc|place_id:ChIJbf8C1yFxdDkR3n12P4DkKt0&mode=driving&language=fr-FR&key=AIzaSyCS90XB-jQMIhQbA2C9vzfWKETNaxpjWJo',
-              // url:'https://maps.googleapis.com/maps/api/distancematrix/json?origins='+result['ans'].forEach(myfunc)+'&destinations='+result['ans'].forEach(myfunc)+'&mode=driving&language=fr-FR&key=AIzaSyCS90XB-jQMIhQbA2C9vzfWKETNaxpjWJo',
-              method: 'GET',
-          } ,function(error,resp, body){
-                  if(!error && resp.statusCode == 200){
-                       var body= JSON.parse(body);
-              
-                      res.status(200).json({
-                      body
-                  });
-              }
-          })
-          arr[i][j]=arr[j][i]=0;
-          }
+        if(i==j){
+          arr[i][j]=0;
+        }
+         else if(arr[i][j] == -1){
+          myfunc(placeIds[i],placeIds[j],i,j);
+          // arr[i][j]=arr[j][i]=ans;
+          
+         }wait((10)*1000).then(() =>tsp(0,n,placeIds) ).then(() => res.status(200).json({
+          result
+      }));   
+          // arr[i][j]=arr[j][i]=0;
+  //         }
       }
   }
     
+
+        
+
   })
 
   module.exports = app;
